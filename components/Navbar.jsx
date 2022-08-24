3de78bd8063009a3d2042aea5e3523d035a9353b
ponochovny <ponochovny@gmail.com>
2022-08-24 19:01:25 +0300

    fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 				<div className={styles.callButton}>
 					<Image src='/img/telephone.png' alt='' width={'32'} height={'32'} />
 				</div>
-				<div className='styles texts'>
+				<div className={styles.texts}>
 					<div className={styles.text}>ORDER NOW!</div>
 					<div className={styles.text}>012 345 678</div>
 				</div>
