e70e8718798fe4254343926d56bcb5bf6091b17c
ponochovny <ponochovny@gmail.com>
2022-08-26 01:41:11 +0300

    button add to cart

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -2,12 +2,15 @@ import Image from 'next/image'
 import { useState } from 'react'
 import styles from '../../styles/Product.module.css'
 import axios from 'axios'
+import { useDispatch } from 'react-redux'
+import { addProduct } from '../../redux/cartSlice'
 
 const Product = ({ pizza }) => {
 	const [price, setPrice] = useState(pizza.prices[0])
 	const [size, setSize] = useState(0)
 	const [quantity, setQuantity] = useState(1)
 	const [extras, setExtras] = useState([])
+	const dispatch = useDispatch()
 
 	const changePrice = (number) => {
 		setPrice(price + number)
@@ -30,6 +33,11 @@ const Product = ({ pizza }) => {
 			setExtras(extras.filter((extra) => extra._id !== option._id))
 		}
 	}
+
+	const handleClick = () => {
+		dispatch(addProduct({ ...pizza, extras, price, quantity }))
+	}
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.left}>
@@ -78,7 +86,9 @@ const Product = ({ pizza }) => {
 						defaultValue={quantity}
 						className={styles.quantity}
 					/>
-					<button className={styles.button}>Add to Cart</button>
+					<button className={styles.button} onClick={handleClick}>
+						Add to Cart
+					</button>
 				</div>
 			</div>
 		</div>
