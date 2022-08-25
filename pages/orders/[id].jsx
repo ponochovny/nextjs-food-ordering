bfc4893de89f44a68d53fb9bb49ead88ed07da21
ponochovny <ponochovny@gmail.com>
2022-08-26 01:52:55 +0300

    Order api and Order page

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -1,13 +1,14 @@
+import axios from 'axios'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import styles from '../../styles/Order.module.css'
 
-const Order = () => {
+const Order = ({ order }) => {
+	const status = order.status
+
 	const [mounted, setMounted] = useState(false)
 	useEffect(() => setMounted(true), [])
 	if (!mounted) return null
-
-	const status = 0
 
 	const statusClass = (index) => {
 		if (index - status < 1) return styles.done
@@ -20,26 +21,30 @@ const Order = () => {
 			<div className={styles.left}>
 				<div className={styles.row}>
 					<table className={styles.table}>
-						<tr className={styles.tr}>
-							<th>Order ID</th>
-							<th>Customer</th>
-							<th>Address</th>
-							<th>Total</th>
-						</tr>
-						<tr>
-							<td>
-								<span className={styles.id}>4651134866</span>
-							</td>
-							<td>
-								<span className={styles.name}>John Doe</span>
-							</td>
-							<td>
-								<span className={styles.address}>Elton st. 212-33 LA</span>
-							</td>
-							<td>
-								<span className={styles.total}>$39.80</span>
-							</td>
-						</tr>
+						<thead>
+							<tr className={styles.tr}>
+								<th>Order ID</th>
+								<th>Customer</th>
+								<th>Address</th>
+								<th>Total</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>
+									<span className={styles.id}>{order._id}</span>
+								</td>
+								<td>
+									<span className={styles.name}>{order.customer}</span>
+								</td>
+								<td>
+									<span className={styles.address}>{order.address}</span>
+								</td>
+								<td>
+									<span className={styles.total}>${order.total}</span>
+								</td>
+							</tr>
+						</tbody>
 					</table>
 				</div>
 				<div className={styles.row}>
@@ -101,13 +106,13 @@ const Order = () => {
 				<div className={styles.wrapper}>
 					<h2 className={styles.title}>CART TOTAL</h2>
 					<div className={styles.totalText}>
-						<b className={styles.totalTextTitle}>Subtotal:</b>$79.60
+						<b className={styles.totalTextTitle}>Subtotal:</b>${order.total}
 					</div>
 					<div className={styles.totalText}>
 						<b className={styles.totalTextTitle}>Discount:</b>$0.00
 					</div>
 					<div className={styles.totalText}>
-						<b className={styles.totalTextTitle}>Total:</b>$79.60
+						<b className={styles.totalTextTitle}>Total:</b>${order.total}
 					</div>
 					<button className={styles.button} disabled>
 						PAID
@@ -116,6 +121,15 @@ const Order = () => {
 			</div>
 		</div>
 	)
+}
+
+export const getServerSideProps = async ({ params }) => {
+	const res = await axios.get(`http://localhost:3000/api/orders/${params.id}`)
+	return {
+		props: {
+			order: res.data,
+		},
+	}
 }
 
 export default Order
